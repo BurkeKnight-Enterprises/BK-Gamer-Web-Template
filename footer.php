7796c0054e01833738f7279b7702da1423ce7721
Burke Knight <github@burkeknight.com>
2016-02-23 20:21:32 -0500

    Initial Upload

---
+++ footer.php
@@ -0,0 +1,14 @@
+<br /><br /><div id="sponsors">
+<div id="sponsorsinner">
+<a href="http://www.burkeknight.com/" target="_blank"><img src="http://www.burkeknight.com/bke_mini.gif" alt="BurkeKnight Enterprises" title="BurkeKnight Enterprises" border="0" width="88" height="31" /></a>
+</div>
+</div>
+
+<div id="footer">
+<div id="footerinner">
+<p>&copy; <?php echo date("Y");?> - BurkeKnight Enterprises<br />
+BK-Gamer Template by <a href="http://www.burkeknight.com/" target="_blank">BurkeKnight Enterprises</a></p>
+
+</div>
+</div>
+</div>
---
+++ header.php
@@ -0,0 +1,66 @@
+<link href="style.css" rel="stylesheet" type="text/css" />
+<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<meta name="viewport" content="width=device-width, user-scalable=no">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+<script language="JavaScript" type="text/javascript">
+<!--
+function framebreakout()
+{
+if (top.location != location) {
+top.location.href = document.location.href ;
+}
+}
+//-->
+</script>
+
+</head>
+<body onload="framebreakout()">
+
+<div id='cssmenu'>
+<ul>
+   <li><a href='index.php'><span>Home</span></a></li>
+<!-- You may remove this support section or replace it with own menu items. -->
+   <li class='has-sub'><a href='#'><span>Support</span></a>
+      <ul>
+         <li><a href='http://www.burkeknight.org/' target='_blank'><span>Support System</span></a></li>
+         <li><a href='http://www.burkeknight.com/contact.php' target='_blank'><span>Contact BurkeKnight Enterprises</span></a></li>
+         <li><a href='http://www.burkeknight.com/terms.php' target='_blank'><span>Terms of Service</span></a></li>
+         <li class='last'><a href='http://www.burkeknight.com/privacy.php' target='_blank'><span>Privacy Policy</span></a></li>
+      </ul>
+   </li>
+<!-- End of suuport menu section. -->
+   <li class='has-sub'><a href='#'><span>Menu 1</span></a>
+      <ul>
+         <li><a href='#'><span>Menu 1.1</span></a></li>
+         <li><a href='#'><span>Menu 1.2</span></a></li>
+         <li><a href='#'><span>Menu 1.3</span></a></li>
+         <li class='last'><a href='#'><span>Menu 1.4</span></a></li>
+      </ul>
+   </li>
+   <li class='has-sub'><a href='#'><span>Menu 2</span></a>
+      <ul>
+         <li><a href='#'><span>Menu 2.1</span></a></li>
+         <li><a href='#'><span>Menu 2.2</span></a></li>
+         <li><a href='#'><span>Menu 2.3</span></a></li>
+         <li class='last'><a href='#'><span>Menu 1.4</span></a></li>
+      </ul>
+   </li>
+   <li class='has-sub'><a href='#'><span>Menu 3</span></a>
+      <ul>
+         <li><a href='#'><span>Menu 3.1</span></a></li>
+         <li><a href='#'><span>Menu 3.2</span></a></li>
+         <li><a href='#'><span>Menu 3.3</span></a></li>
+         <li class='last'><a href='#'><span>Menu 1.4</span></a></li>         
+      </ul>
+   </li>
+   <li class='last'><a href='#'><span>Contact</span></a></li>
+</ul>
+</div>
+<br /><br /><div id="head">
+<div class="headinside">
+ <h2><a href="/index.php">BK-Gamer</a></h2><br /><br /><h4>Web Template</h4>
+   </div>
+</div>
+
+<div id="wrapper">
